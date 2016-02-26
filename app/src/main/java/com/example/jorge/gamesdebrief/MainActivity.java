279d636107a0796b14bd13c1f99a074b5b5bf689
jorge <jorgeafs99@gmail.com>
2016-02-26 14:18:17 +0100

    Montando el mainActivity.java
    Preparando todo para iniciar

--- app/src/main/java/com/example/jorge/gamesdebrief/MainActivity.java
+++ app/src/main/java/com/example/jorge/gamesdebrief/MainActivity.java
@@ -4,24 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    //Constants
+    private static final String MENU_JUEGO = "menu_juego";
+    private static final String MENU_PRINCIPAL = "menu_principal";
+    private static final String DETALLE_PARTIDA = "detalle_partida";
+
+    //Variables
+    MenuPrincipal menuPrincipal;
+    MenuJuego menuJuego;
+    DetallePartida detalles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (findViewById(R.id.singleContainer)!=null){
-            if(savedInstanceState != null){
-
-            }else{
-
-            }
-        }else {
-            if(savedInstanceState != null){
-
-            }else{
-
-            }
+        if (findViewById(R.id.singleContainer)!= null && savedInstanceState == null){
+            //primera carga de la app con el movil en posicion vertical y sin ningun dato salvado
+            menuPrincipal = new MenuPrincipal();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.menuPrincipal, menuPrincipal,MENU_PRINCIPAL)
+                    .commit();
+        } else if (findViewById(R.id.singleContainer)!=null && savedInstanceState != null){
+            //movil en posicion vertical con datos en el saveInstaceState, debemos ver que fragment lanzamos
+        }else if(findViewById(R.id.singleContainer)== null && savedInstanceState != null){
+            //movil en posicion horizontal y sin ningun dato salvado
+        } else {
+            //movil en posicion horizontal y con datos salvados
         }
     }
 }
