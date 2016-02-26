66c22554a982c2d4bc70cc1af70904d55ed7c62d
jorge <jorgeafs99@gmail.com>
2016-02-26 13:50:48 +0100

    Pre-DB basic classes
    This are the contract class and helper
    
    To Do:
    -Dal class and interface for callback

---
+++ app/src/main/java/com/example/jorge/gamesdebrief/PartidasDB.java
@@ -0,0 +1,54 @@
+package com.example.jclozano.proyectito;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by jclozano on 24/02/2016.
+ */
+public class PartidasDB  {
+    private  PartidasDB () {}
+
+    public static final class Tipo implements BaseColumns {
+        private Tipo () {}
+        public static final String TIPO_TABLE_NAME = "table_tipo";
+        public static final String TIPO_ID = "tipo_id";
+        public static final String TIPO_NOMBRE = "tipo_nombre";
+    }
+
+    public static final class Juego implements BaseColumns {
+        private Juego () {}
+        public static final String JUEGO_TABLE_NAME = "table_juego";
+        public static final String JUEGO_ID = "juego_id";
+        public static final String JUEGO_NOMBRE = "juego_nombre";
+        public static final String JUEGO_IS_SINGLE_PLAYER = "juego_is_single_player";
+        public static final String JUEGO_ID_TIPO = "juego_id_tipo";
+    }
+
+    public static final class ModoPartida implements BaseColumns {
+        private ModoPartida () {}
+        public static final String MODO_PARTIDA_TABLE_NAME = "table_modo_partida";
+        public static final String MODO_PARTIDA_ID = "modo_partida_id";
+        public static final String MODO_PARTIDA_NOMBRE = "modo_partida_nombre";
+    }
+
+    public static final class Mapa implements BaseColumns {
+        private Mapa () {}
+        public static final String MAPA_TABLE_NAME = "table_mapa";
+        public static final String MAPA_ID = "mapa_id";
+        public static final String MAPA_NOMBRE = "mapa_nombre";
+    }
+
+    public static final class Partida implements BaseColumns {
+        private Partida (){}
+        public static final String PARTIDA_TABLE_NAME = "table_partida";
+        public static final String PARTIDA_ID = "partida_id";
+        public static final String PARTIDA_ID_JUEGO = "partida_id_juego";
+        public static final String PARTIDA_ID_MODO_PARTIDA = "partida_id_modo_partida";
+        public static final String PARTIDA_ID_MAPA = "partida_id_mapa";
+        public static final String PARTIDA_NUMERO_JUGADORES = "partida_numero_jugadores";
+        public static final String PARTIDA_NUMERO_JUGADORES_ALIADOS = "partida_numero_jugadores_aliados";
+        public static final String PARTIDA_NUMERO_JUGADORES_ENEMIGOS = "partida_numero_jugadores_enemigos";
+        public static final String PARTIDA_ES_GANADA = "partida_es_ganada";
+        public static final String PARTIDA_DESCRIPCION = "descripcion";
+    }
+}
---
+++ app/src/main/java/com/example/jorge/gamesdebrief/PartidasDBHelper.java
@@ -0,0 +1,80 @@
+package com.example.jclozano.proyectito;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by jclozano on 24/02/2016.
+ */
+public class PartidasDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "partidasDB.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public PartidasDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+PartidasDB.Tipo.TIPO_TABLE_NAME
+                +" ( "+
+                    PartidasDB.Tipo.TIPO_ID+" INTEGER PRIMARY KEY AUTOINCREMENT "
+                    +", "+PartidasDB.Tipo.TIPO_NOMBRE+" TEXT "
+                +");");
+        db.execSQL("CREATE TABLE "+ PartidasDB.ModoPartida.MODO_PARTIDA_TABLE_NAME
+                +" ( "+
+                    PartidasDB.ModoPartida.MODO_PARTIDA_ID+" INTEGER PRIMARY KEY AUTOINCREMENT "
+                    +", "+PartidasDB.ModoPartida.MODO_PARTIDA_NOMBRE+" TEXT "
+                +" );");
+        db.execSQL("CREATE TABLE "+PartidasDB.Mapa.MAPA_TABLE_NAME
+                +" ( "+
+                    PartidasDB.Mapa.MAPA_ID+" INTEGER PRIMARY KEY AUTOINCREMENT "
+                    +", "+PartidasDB.Mapa.MAPA_NOMBRE+" TETX "
+                +" );");
+        db.execSQL("CREATE TABLE "+PartidasDB.Juego.JUEGO_TABLE_NAME
+                +" ( "+
+                    PartidasDB.Juego.JUEGO_ID+" INTEGER PRIMARY KEY AUTOINCREMENT "
+                    +", "+PartidasDB.Juego.JUEGO_NOMBRE+" TETX "
+                    +", "+PartidasDB.Juego.JUEGO_IS_SINGLE_PLAYER+" NUMERIC "
+                    +", "+PartidasDB.Juego.JUEGO_ID_TIPO+" INTEGER "
+                    +", FOREIGN KEY ( "+ PartidasDB.Juego.JUEGO_ID_TIPO+" ) REFERENCES "+ PartidasDB.Tipo.TIPO_TABLE_NAME+" ( "+ PartidasDB.Tipo.TIPO_ID+" ) "
+                    +", CHECK ( "+ PartidasDB.Juego.JUEGO_IS_SINGLE_PLAYER +" IN (0,1))"
+                +" );");
+
+        db.execSQL("CREATE TABLE " + PartidasDB.Partida.PARTIDA_TABLE_NAME
+                + " ( " +
+                PartidasDB.Partida.PARTIDA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT "
+                + ", " + PartidasDB.Partida.PARTIDA_ID_JUEGO + " INTEGER "
+                + ", " + PartidasDB.Partida.PARTIDA_ID_MAPA + " INTEGER "
+                + ", " + PartidasDB.Partida.PARTIDA_ID_MODO_PARTIDA + " INTEGER "
+                + ", " + PartidasDB.Partida.PARTIDA_NUMERO_JUGADORES + " INTEGER "
+                + ", " + PartidasDB.Partida.PARTIDA_NUMERO_JUGADORES_ALIADOS + " INTEGER "
+                + ", " + PartidasDB.Partida.PARTIDA_NUMERO_JUGADORES_ENEMIGOS + " INTEGER "
+                + ", " + PartidasDB.Partida.PARTIDA_ES_GANADA + " NUMERIC "
+                + ", " + PartidasDB.Partida.PARTIDA_DESCRIPCION + " TEXT "
+                + ", FOREIGN KEY ( " + PartidasDB.Partida.PARTIDA_ID_JUEGO + " ) REFERENCES " + PartidasDB.Juego.JUEGO_TABLE_NAME + " ( " + PartidasDB.Juego.JUEGO_ID + " ) "
+                + ", FOREIGN KEY ( " + PartidasDB.Partida.PARTIDA_ID_MAPA + " ) REFERENCES " + PartidasDB.Mapa.MAPA_TABLE_NAME + " ( " + PartidasDB.Mapa.MAPA_ID + " ) "
+                + ", FOREIGN KEY ( " + PartidasDB.Partida.PARTIDA_ID_MODO_PARTIDA + " ) REFERENCES " + PartidasDB.ModoPartida.MODO_PARTIDA_TABLE_NAME + " ( " + PartidasDB.ModoPartida.MODO_PARTIDA_ID + " ) "
+                + ", CHECK ( " + PartidasDB.Partida.PARTIDA_ES_GANADA + " IN (0,1))"
+                + ", CHECK ( " + PartidasDB.Partida.PARTIDA_NUMERO_JUGADORES + " == ( " + PartidasDB.Partida.PARTIDA_NUMERO_JUGADORES_ALIADOS + " + " + PartidasDB.Partida.PARTIDA_NUMERO_JUGADORES_ENEMIGOS + " ) "
+                + " );");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        android.util.Log.w("Constants", "Upgrading database, which will destroy allold data");
+        db.execSQL("DROP TABLE IF EXISTS " + PartidasDB.Mapa.MAPA_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PartidasDB.Tipo.TIPO_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PartidasDB.ModoPartida.MODO_PARTIDA_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PartidasDB.Juego.JUEGO_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PartidasDB.Partida.PARTIDA_TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
