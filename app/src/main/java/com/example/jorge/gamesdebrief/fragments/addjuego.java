00570ee67eb5599bb28f1e777b763f56b13f7a66
jorge <jorgeafs99@gmail.com>
2016-03-01 21:28:00 +0100

    Terminando addJuego
    LA logica de añadir juego esta casi completa.....

--- app/src/main/java/com/example/jorge/gamesdebrief/fragments/addjuego.java
+++ app/src/main/java/com/example/jorge/gamesdebrief/fragments/addjuego.java
@@ -1,5 +1,6 @@
 package com.example.jorge.gamesdebrief.fragments;
 
+import android.app.DialogFragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,11 +8,17 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Spinner;
 
 import com.example.jorge.gamesdebrief.R;
+import com.example.jorge.gamesdebrief.clasesDeApoyo.DatosSpiner;
+import com.example.jorge.gamesdebrief.clasesDeApoyo.MultiAdaptador;
+
+import java.util.List;
 
 
 /**
@@ -28,6 +35,10 @@ public class Addjuego extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private static final String GENERO = "genero";
+    private static final String MODO = "modo";
+    private static final String MAPA = "mapa";
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -37,6 +48,11 @@ public class Addjuego extends Fragment {
     private Spinner mapa;
     private Spinner modo;
     private GridView grid;
+    private Button añadeJuego;
+    private Context actualContex;
+    private MultiAdaptador adaptadorGenero;
+    private MultiAdaptador adaptadorModo;
+    private MultiAdaptador adaptadorMapa;
 
     private OnFragmentInteractionListener mListener;
 
@@ -75,22 +91,111 @@ public class Addjuego extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_addjuego, container, false);
-
-        //preparaSpinners(view);
+        nombreJuego = (EditText) view.findViewById(R.id.nombreJuego);
+        preparaGenero(view,inflater);
+        preparaModo(view,inflater);
+        preparaMapa(view, inflater);
+        preparaButton(view);
 
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    private void preparaGenero(View view, LayoutInflater inflater) {
+        genero = (Spinner) view.findViewById(R.id.genero);
+        adaptadorGenero = new MultiAdaptador(mListener.getDatos(GENERO),inflater,actualContex);
+        genero.setAdapter(adaptadorGenero);
+        genero.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (adaptadorGenero.getItem(position).getId() == -1) {
+                    DialogFragment añadir = DialogAñadir.newInstance(GENERO);
+                    añadir.setShowsDialog(true);
+                    añadir.show(getFragmentManager(), "dialog");
+                } else if (adaptadorGenero.getItem(position).getId() == 0) {
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void preparaModo(View view, LayoutInflater inflater){
+        modo = (Spinner) view.findViewById(R.id.nuevoModo);
+        adaptadorModo = new MultiAdaptador(mListener.getDatos(MODO),inflater,actualContex);
+        modo.setAdapter(adaptadorModo);
+        modo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (adaptadorModo.getItem(position).getId() == -1) {
+                    DialogFragment añadir = DialogAñadir.newInstance(MODO);
+                    añadir.setShowsDialog(true);
+                    añadir.show(getFragmentManager(), "dialog");
+                } else if (adaptadorModo.getItem(position).getId() == 0) {
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void preparaMapa(View view, LayoutInflater inflater){
+        mapa = (Spinner) view.findViewById(R.id.nuevoMapa);
+        adaptadorMapa = new MultiAdaptador(mListener.getDatos(MAPA),inflater,actualContex);
+        mapa.setAdapter(adaptadorMapa);
+        mapa.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (adaptadorMapa.getItem(position).getId() == -1) {
+                    DialogFragment añadir = DialogAñadir.newInstance(MAPA);
+                    añadir.setShowsDialog(true);
+                    añadir.show(getFragmentManager(), "dialog");
+                } else if (adaptadorMapa.getItem(position).getId() == 0) {
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void preparaButton(View view){
+        añadeJuego = (Button) view.findViewById(R.id.añadirJuegoNuevo);
+        añadeJuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.insertaDatos(nombreJuego.getText().toString(),);
+            }
+        });
+    }
+
+/*    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
+    }*/
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        actualContex = context;
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
@@ -116,7 +221,6 @@ public class Addjuego extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        public List<DatosSpiner> getDatos(String nombreDato);
     }
 }
