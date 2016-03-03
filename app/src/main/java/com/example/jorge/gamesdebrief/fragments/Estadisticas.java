6b114fc3c1808bed7bc5e467af51f6490a5dcc59
jorgeafs <jorgeafs99@gmail.com>
2016-03-03 11:11:24 +0100

    Estadisticas casi terminada
    Falta añadir accion a los botones para que rellenen los listview

--- app/src/main/java/com/example/jorge/gamesdebrief/fragments/Estadisticas.java
+++ app/src/main/java/com/example/jorge/gamesdebrief/fragments/Estadisticas.java
@@ -7,8 +7,16 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.jorge.gamesdebrief.R;
+import com.example.jorge.gamesdebrief.clasesDeApoyo.DatosSpiner;
+import com.example.jorge.gamesdebrief.clasesDeApoyo.MultiAdaptador;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +36,19 @@ public class Estadisticas extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private Button juego;
+    private Button genero;
+    private Button modo;
+    private TextView tipoDato;
+    private ListView listaDato;
+    private ListView listaGanada;
+    private ListView listaEmpatada;
+    private ListView listaPerdida;
+    private MultiAdaptador adaptadorDatos;
+    private MultiAdaptador adaptadorGanada;
+    private MultiAdaptador adaptadorEmpatada;
+    private MultiAdaptador adaptadorPerdida;
+    private Context actualContext;
     private OnFragmentInteractionListener mListener;
 
     public Estadisticas() {
@@ -65,7 +86,69 @@ public class Estadisticas extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_estadisticas, container, false);
+        View view = inflater.inflate(R.layout.fragment_estadisticas, container, false);
+        preparaBotonJuego(view, inflater);
+        preparaBotonGenero(view, inflater);
+        preparaBotonModo(view, inflater);
+        preparaListaDato(view, inflater);
+        preparaListaGanada(view, inflater);
+        preparaListaEmpatada(view, inflater);
+        preparaListaPerdida(view, inflater);
+        return view;
+    }
+
+    private void preparaBotonJuego(View view, LayoutInflater inflater){
+        juego = (Button) view.findViewById(R.id.botonJuego);
+        juego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void preparaBotonGenero(View view, LayoutInflater inflater){
+        genero = (Button) view.findViewById(R.id.botonGenero);
+        genero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void preparaBotonModo(View view, LayoutInflater inflater){
+        modo = (Button) view.findViewById(R.id.botonModo);
+        modo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void preparaListaDato(View view, LayoutInflater inflater){
+        listaDato = (ListView) view.findViewById(R.id.listaDatos);
+        adaptadorDatos = new MultiAdaptador(new ArrayList<DatosSpiner>(), inflater, actualContext);
+        listaDato.setAdapter(adaptadorDatos);
+    }
+
+    private void preparaListaGanada(View view, LayoutInflater inflater){
+        listaGanada = (ListView) view.findViewById(R.id.listaPartidasGanadas);
+        adaptadorGanada = new MultiAdaptador(new ArrayList<DatosSpiner>(),inflater,actualContext);
+        listaGanada.setAdapter(adaptadorGanada);
+    }
+
+    private void preparaListaEmpatada(View view, LayoutInflater inflater){
+        listaEmpatada = (ListView) view.findViewById(R.id.listaPartidasEmpatadas);
+        adaptadorEmpatada = new MultiAdaptador(new ArrayList<DatosSpiner>(),inflater,actualContext);
+        listaEmpatada.setAdapter(adaptadorEmpatada);
+    }
+
+    private void preparaListaPerdida(View view, LayoutInflater inflater){
+        listaPerdida = (ListView) view.findViewById(R.id.listaPartidasPerdidas);
+        adaptadorPerdida = new MultiAdaptador(new ArrayList<DatosSpiner>(),inflater,actualContext);
+        listaPerdida.setAdapter(adaptadorPerdida);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -78,6 +161,7 @@ public class Estadisticas extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        actualContext = context;
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
